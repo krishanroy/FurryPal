b4a25ab67fdc31a6962dfece861e0edb0d134b85
krishanroy <krishanroy@pursuit.org>
2025-07-03 11:55:08 -0400

    add DetailsScreen.kt and DetailsViewModel.kt implementaton

--- app/src/main/java/com/krishan/furrypal/ui/details/DetailsScreen.kt
+++ app/src/main/java/com/krishan/furrypal/ui/details/DetailsScreen.kt
@@ -1,18 +1,23 @@
 package com.krishan.furrypal.ui.details
 
+import android.widget.Toast
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,29 +31,50 @@ import com.krishan.furrypal.Screen
 fun DetailScreen(navController: NavHostController) {
     val breedName = navController.currentBackStackEntry?.toRoute<Screen.Details>()?.breedName.toString()
     val viewModel: DetailsViewModel = viewModel()
-    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-        Column {
-            Surface(
-                shape = RoundedCornerShape(12.dp), color = Color.LightGray,
-                modifier = Modifier
-                    .padding(16.dp)
-                    .size(275.dp)
-            ) {
-                AsyncImage(
-                    model = "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-                    contentDescription = "test image"
-                )
+    viewModel.handleIntent(intent = DetailsScreenIntent.FetchDogImage(breedName))
+    val imageUrlStateFlow = viewModel.dogImageStateFlow.collectAsState().value
+
+    when (imageUrlStateFlow) {
+        is DetailsStateUi.Loading -> {
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                CircularProgressIndicator()
             }
-            Text(
-                breedName,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.clickable(
-                    onClick = {
-                        navController.popBackStack()
+        }
+
+        is DetailsStateUi.Error -> {
+            Toast.makeText(LocalContext.current, "Something went wrong!", Toast.LENGTH_SHORT).show()
+        }
+
+        is DetailsStateUi.Success -> {
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                Column(verticalArrangement = Arrangement.Center) {
+                    Surface(
+                        shape = RoundedCornerShape(12.dp), color = Color.LightGray,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .size(275.dp)
+                    ) {
+                        AsyncImage(
+                            model = imageUrlStateFlow.imageUrl,
+                            contentDescription = "test image"
+                        )
                     }
-                )
-            )
+                    Text(
+                        breedName,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.clickable(
+                            onClick = {
+                                navController.popBackStack()
+                            }
+                        )
+                    )
+                }
+            }
+        }
+
+        is DetailsStateUi.NoImagesForThatBreed -> {
+            Toast.makeText(LocalContext.current, "No image available for that breed", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/krishan/furrypal/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/krishan/furrypal/ui/details/DetailsViewModel.kt
@@ -1,21 +1,51 @@
 package com.krishan.furrypal.ui.details
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.krishan.furrypal.data.remote.FurryService
+import com.krishan.furrypal.data.remote.RetrofitSingleton
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class DetailsViewModel : ViewModel() {
     private val _dogImageMutableStateFlow = MutableStateFlow<DetailsStateUi>(DetailsStateUi.Loading)
-    val dogImageStateFlow = _dogImageMutableStateFlow
+    val dogImageStateFlow: StateFlow<DetailsStateUi> = _dogImageMutableStateFlow
+
+    fun handleIntent(intent: DetailsScreenIntent) {
+        when (intent) {
+            is DetailsScreenIntent.FetchDogImage -> getImageDetails(breedName = intent.breedName)
+        }
+    }
 
     private fun getImageDetails(breedName: String) {
-
+        viewModelScope.launch(Dispatchers.IO) {
+            val service = RetrofitSingleton.retrofit.create(FurryService::class.java)
+            try {
+                val response = service.getImagesByBreed(breed = breedName)
+                if (response.isSuccessful && response.body() != null) {
+                    _dogImageMutableStateFlow.value =
+                        DetailsStateUi.Success(imageUrl = response.body()?.message?.last().toString())
+                } else {
+                    _dogImageMutableStateFlow.value = DetailsStateUi.NoImagesForThatBreed
+                }
+            } catch (exception: Exception) {
+                _dogImageMutableStateFlow.value = DetailsStateUi.Error(exception = exception)
+            }
+        }
     }
 }
 
 sealed class DetailsStateUi {
     object Loading : DetailsStateUi()
     data class Success(val imageUrl: String) : DetailsStateUi()
+    data object NoImagesForThatBreed : DetailsStateUi()
     data class Error(val exception: Exception) : DetailsStateUi()
+}
+
+sealed class DetailsScreenIntent {
+    data class FetchDogImage(val breedName: String) : DetailsScreenIntent()
 }
 
 
